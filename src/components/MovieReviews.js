ac1734e772f06460488776bcd430d7188e76efad
Alberto <ajsaenz1091@gmail.com>
2021-06-04 12:46:53 -0400

    commit tree 55a273ed31fe23c61db529d9cf891f7a42373496

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,29 @@
 // Code MovieReviews Here
+import React from 'react'
+
+
+
+const MovieReviews = ({reviews}) => {
+
+    let genReview = () => {
+        return reviews.map((review, idx) => {
+            return <div key={idx} className="card">
+                        <img src={review.multimedia.src} alt="Avatar" style={{ width: '100%' }} />
+                        <div className="container">
+                            <h4>By:{review.byline} --- <b>{review.headline}</b></h4>
+                            <h5>Posted on: {review.date_updated}</h5>
+                            <p>{review.summary_short}</p>
+                        </div>
+                    </div>
+        })
+    }
+
+    return (
+        <div className="review-list">
+            {genReview()}
+        </div>
+    )
+}
+
+export default MovieReviews;
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,23 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+    + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: []
+    }
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
+
+export default SearchableMovieReviewsContainer;
